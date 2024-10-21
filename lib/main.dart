7a2549259876c0b83ec0ad51736e0e0bf6a0ffce
FunGi2410 <nguyennhanit2410@gmail.com>
2024-10-21 22:39:27 +0700

    add home screen and inventory screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,65 @@
-import 'dart:async';
-
-import 'package:smart_farm_iot/auth/login_screen.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:smart_farm_iot/home_screen.dart';
+import 'views/home_screen.dart';
+import 'views/inventory_screen.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp();
-
-  runApp(const MyApp());
+void main() {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Bottom Navigation Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: BottomNavScreen(),
+    );
+  }
+}
+
+class BottomNavScreen extends StatefulWidget {
+  @override
+  _BottomNavScreenState createState() => _BottomNavScreenState();
+}
+
+class _BottomNavScreenState extends State<BottomNavScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = [
+    HomeScreen(),
+    InventoryScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Smart Farm',
-      home: HomeScreen()
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bottom Navigation Example'),
+      ),
+      body: _screens[_selectedIndex], 
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.inventory),
+            label: 'Inventory',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,  
+        onTap: _onItemTapped,            
+      ),
     );
   }
 }
